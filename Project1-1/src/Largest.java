36b7892261a84845269cb2efd7e52577e4ffb14c
BobbyLeigh <70552823+BobbyLeigh@users.noreply.github.com>
2020-09-22 06:28:29 -0400

    2

---
+++ Project1-1/src/Largest.java
@@ -0,0 +1,19 @@
+public class Largest {
+static int[] a={2,1,5,3};
+public static void main(String args[])
+{
+long start=System.currentTimeMillis();
+int largest=getLargestElement(a,0);
+System.out.println(largest);
+long end=System.currentTimeMillis();
+System.out.println("Execution Time : "+(end-start));
+}
+private static int getLargestElement(int[] a,int position)
+{
+if(position == a.length-1)
+return a[position];
+else
+return Math.max(a[position], getLargestElement(a, position+1));
+}
+  
+}
